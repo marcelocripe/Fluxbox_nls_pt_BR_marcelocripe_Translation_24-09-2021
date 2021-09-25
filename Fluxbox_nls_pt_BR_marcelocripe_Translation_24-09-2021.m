15cda334f192d2a708a5f78283962e1c95e83cb2
marcelocripe <69278712+marcelocripe@users.noreply.github.com>
2021-09-24 23:05:51 -0300

    Translation.m
    Hello Mr Mathias Gumz,
    
    Please accept my review of Fluxbox_nls_pt_BR_marcelocripe_Translation_24-09-2021.m. The main actions are: humanization of automatically translated texts, improvements in the formation of texts, in addition to the necessary adaptations and adjustments so that they can make complete sense in my language.
    Fluxbox is also used by Tiny Core Linux and antiX Linux.
    
    Thank you so much for programming and maintaining Fluxbox.
    
    - - - - -
    
    Olá Senhor Mathias Gumz,
    
    Por favor, aceite a minha revisão do Fluxbox_nls_pt_BR_marcelocripe_Translation_24-09-2021.m. As principais ações são: humanização dos textos traduzidos automaticamente, melhoramentos na formação dos textos, além das adaptações e adequações necessárias para poderem fazer sentido completo em meu idioma.
    O Fluxbox é utilizado também pelo Tiny Core Linux e no antiX Linux.
    
    Muito obrigado por programar e manter o Fluxbox.

---
+++ Fluxbox_nls_pt_BR_marcelocripe_Translation_24-09-2021.m
@@ -0,0 +1,281 @@
+$ codeset=UTF-8
+
+$set 1 #Align
+
+1 Embaixo no Centro
+2 Embaixo à Esquerda
+3 Embaixo à Direita
+4 Horizontal
+5 À Esquerda
+6 À Esquerda Embaixo
+7 À Esquerda no Centro
+8 À Esquerda em Cima
+9 Relativo
+10 À Direita
+11 À Direita Embaixo
+12 À Direita no Centro
+13 À Direita em Cima
+14 Em Cima no Centro
+15 Em Cima à Esquerda
+16 Em Cima à Direita
+17 Vertical
+18 No Centro
+19 Em Cima
+20 Embaixo
+
+$set 2 #BaseDisplay
+
+1 Abortando... despejando o núcleo\n
+2 Desligando\n
+3 %s:      sinal %d recebido\n
+
+$set 3 #Common
+
+1 Transparência
+2 Ocultar Automaticamente
+3 Compilado
+4 Opções de Compilação
+5 Compilador
+6 Versão do Compilador
+7         init
+8         keys
+9          nls
+10        menu
+20  windowmenu
+11       style
+12 Padrões
+13 desativado
+14 Erro
+15 Versão do Fluxbox
+16 Maximizar Sobre
+17 Revisão do GIT
+18 Visível
+19 Não há uma opção de fundo especificada para este tema.\nPor favor, consulte o manual ou leia as Perguntas Frequentes [FAQ].
+
+$set 4 #Configmenu
+
+1 Suavização das Bordas [AntiAlias]
+2 Sobrepor a Janela Automaticamente
+3 Clicar para Sobrepor a Janela
+4 Clicar para Focar a Janela
+5 Decorar as Janelas Temporárias
+6 A Roda do Rato/Mouse Altera a Área de Trabalho
+7 Focar a Janela ao Alterar a Área de Trabalho
+8 Modelo do Foco
+9 Focar em Novas Janelas
+10 Maximização Total
+11 Simulação de Cores em Imagens
+12 A Movimentação Opaca de Janelas
+14 O Foco Segue o Rato/Mouse
+15 Áreas de Trabalho Contínuas
+16 Forçar a Pseudotransparência
+17 Transparência do Menu
+18 Transparência
+19 Transparência da Janela com Foco
+20 Transparência da Janela sem Foco
+21 Opções de Abas
+22 Abas na Barra de Título
+23 Largura da Aba Externa
+24 Clicar para Focar a Aba
+25 O Rato/Mouse Foca a Aba
+26 Opções de Maximização
+27 Ignorar Incremento do Redimensionamento
+28 Desativar a Movimentação
+29 Desativar o Redimensionamento
+
+$set 5 #Ewmh
+
+1 Fatal: A memória não é suficiente, não é possível alocar memória para a lista de clientes do EWMH
+
+$set 6 #FbTkError
+
+1 Atenção! Altura > 3200, definindo a Altura = 3200
+2 Atenção! Largura > 3200, definindo a  Largura = 3200
+3 Aviso: Não é possível carregar a fonte reserva
+4 Erro de alocação.
+5 Erro na criação do pixmap
+6 Não é possível criar o XImage
+7 Falha na leitura
+8 Aviso: Não foi possível encontrar um formato válido para a transparência.
+9 Aviso: Não foi possível criar uma imagem com transparência.
+10 Aviso: Não foi possível criar um pixmap com transparência.
+11 Não foi possível encontrar um formato para a tela (%d)
+12 A memória não é suficiente
+13 A memória não é suficiente para alocar o buffer da cor azul.
+14 A memória não é suficiente para alocar o buffer da cor verde.
+15 A memória não é suficiente para alocar o buffer da cor vermelha.
+16 Falha na leitura do item do tema
+17 O visual não é suportado
+18 Configurando o valor padrão
+
+$set 7 #Fluxbox
+
+1 O nome do arquivo rc não é válido!
+2 Não foi possível carregar o arquivo de grupos
+3 Não foi possível carregar o banco de dados
+4 Realizando uma nova tentativa com
+5 Atenção! Não foi encontrada uma tela para onde mapear a janela!
+6 Não é possível criar o diretório %s
+7 Não foram encontradas as telas para serem gerenciadas.\nCertifique-se de que não há outro gerenciador de janelas em execução.
+8 Erro ao analisar a expressão regular
+9 Fatal! Só é possível haver uma instância da classe fluxbox.
+10 Não é possível conectar-se ao servidor X.\nCertifique-se de que o X foi iniciado antes do Fluxbox.
+11 Aviso: O servidor X não suporta as traduções [locale]
+12 Aviso: Não é possível definir os modificadores traduzidos/localizados
+
+$set 8 #Gnome
+
+1 Fatal: A memória não é suficiente, não é possível alocar memória para a lista de clientes do GNOME
+
+$set 9 #Keys
+
+1 Teclas: Erro na linha
+2 Teclas: Não foi possível fundir a árvore de atalhos de teclas!
+3 Teclas: A tecla/modificador não é válido na linha
+
+$set 10 #Menu
+
+1 Configurações
+2 Menu Padrão do Fluxbox
+3 Sair
+4 Janelas Minimizadas
+5 Camada...
+6 No Monitor...
+7 Posicionamento
+8 Recarregar as Configurações
+9 Reiniciar
+10 Aviso: As variáveis [codificação] estão desbalanceadas
+
+$set 11 #Remember
+
+1 Decorações
+2 Dimensões
+3 Ir para Área de Trabalho
+4 Camada
+5 Memorizar...
+6 Posição
+7 Salvar ao Sair
+8 Enrolado
+9 Sempre Visível
+10 Item de Aplicativo Desconhecido
+11 Área de Trabalho
+12 Monitor
+13 Transparência
+
+$set 12 #Screen
+
+1 BScreen::BScreen: Ocorreu um erro ao interrogar o servidor X.\n	Outro gerenciador de janelas está em execução
+2 L: %4d x A: %4d
+3 BScreen::BScreen: O gerenciando da tela %d está utilizando o visual 0x%lx, com a profundidade %d\n
+4 L: %04d x A: %04d
+
+$set 13 #Slit
+
+1 Clientes
+2 Circular para Baixo
+3 Circular para Cima
+4 Direção do Slit:
+5 Camada do Slit
+6 O Slit no Monitor
+7 Posicionamento do Slit
+8 Slit
+9 Salvar a Lista do Slit
+
+$set 14 #Toolbar
+
+1 Renomear a Área de Trabalho Atual
+2 Modo da Barra de Janelas
+3 Todas as Janelas
+4 Apenas as Janelas Minimizadas
+5 Nenhuma
+6 Área de Trabalho
+7 Apenas as Janelas Minimizadas da Área de Trabalho
+8 Camada da Barra de Ferramentas
+9 Barra de Ferramentas no Monitor
+10 Posicionamento da Barra de Ferramentas
+11 Barra de Ferramentas
+12 Largura da Barra de Ferramentas em Porcentagem
+13 Relógio: 24h
+14 Relógio: 12h
+15 Editar o Formato do Relógio
+16 Exibir as Imagens
+17 Todas as Janelas Exceto as Minimizadas
+18 Sem as Janelas Minimizadas da Área de Trabalho
+
+$set 15 #Window
+
+1 Sem Nome
+
+$set 16 #Windowmenu
+
+1 Fechar
+2 Minimizar
+3 Camada
+4 Por Baixo
+5 Maximizar
+6 Por Cima
+7 Enviar Para...
+8 Enrolar
+9 Sempre Visível
+10 Forçar o Fechamento
+11 Usar Valores Padrão
+12 Definir o Título da Janela
+
+$set 17 #Workspace
+
+1 Área de Trabalho %d
+2 Áreas de Trabalho
+3 Nova Área de Trabalho
+4 Remover a Última Área de Trabalho
+
+$set 18 #fbsetroot
+
+1 Erro: É necessário especificar um dos seguintes argumentos: -solid, -mod, -gradient\n
+2 Não foi possível criar os átomos de pixmap, desistindo!
+3 -display <string>      conexão com monitor\n\
+-mod <x> <y>             altera o padrão\n\
+-foreground, -fg <cor>   altera a cor do primeiro plano\n\
+-background, -bg <cor>   altera cor de fundo\n\n\
+-gradient <textura>      textura do gradiente\n\
+-from <cor>              cor de início do gradiente\n\
+-to <cor>                cor de fim do gradiente\n\n\
+-solid <cor>             cor sólida\n\n\
+-help                    exibe esta ajuda e sai\n
+
+$set 19 #main
+
+1 Erro: O comando '-display' requer um argumento
+2 A Alocação Não é Válida
+3 A Conversão Não é Válida
+4 Fora dos Limites
+5 Erro de Execução
+6 Exceção Padrão
+7 Erro desconhecido
+8 Erro: O comando '-log' requer um argumento
+9 Arquivo de Registro
+10 Salvando o Registro [log] em
+11 Erro: O comando '-rc' requer um argumento
+12 Erro: O comando '-screen' requer um argumento
+13 Fluxbox %s: (c) %s Equipe do Fluxbox\n\
+Página eletrônica: http://www.fluxbox.org/\n\n\
+-display <string>\t\tutilizar a conexão com o monitor.\n\
+-screen <all|int,int,int>\texecutar apenas nas telas especificadas.\n\
+-no-slit\t\t\tnão fornece um slit.\n\
+-no-toolbar\t\t\tnão fornece uma barra de ferramentas.\n\
+-rc <string>\t\t\tutilizar o arquivo de configuração alternativo.\n\
+-version\t\t\texibir a versão e sair.\n\
+-info\t\t\t\texibir algumas informações úteis.\n\
+-list-commands\t\t\tlistar todos os comandos de teclas válidos.\n\
+-sync\t\t\t\tsincronizar com o servidor X para depuração.\n\
+-log <nomedoarquivo>\t\t\tsalvar os registros de saída em um arquivo.\n\
+-help\t\t\t\texibir esta ajuda e sair.\n\n
+14 Aviso: não foi possível definir a variável de ambiente 'DISPLAY'
+
+$set 20 #layers
+
+1 Acima da Doca
+2 Embaixo
+3 Área de Trabalho
+4 Doca
+5 Normal
+6 Em Cima
